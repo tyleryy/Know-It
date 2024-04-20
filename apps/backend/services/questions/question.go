4c9162bf4f0b89fcea475343bb736a919031eb84
Katy Huang <katyhuang360166@gmail.com>
2024-04-20 11:11:46 -0700

    refactor: change  up roomId type

--- apps/backend/services/questions/question.go
+++ apps/backend/services/questions/question.go
@@ -5,8 +5,10 @@ import "github.com/google/uuid"
 // Type of our question
 type Question struct {
 	ID          uuid.UUID `json:"id"`
-	RoomID      uuid.UUID `json:"roomID"`
+	RoomID      string    `json:"roomID"`
 	Prompt      string    `json:"prompt"`
 	Answer      string    `json:"answer"`
 	Explanation string    `json:"explanation"`
 }
+
+var separator = "------SEPARATOR------"
