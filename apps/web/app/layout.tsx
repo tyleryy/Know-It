0dcdcad63450fa1c68a93e77f8cba326ea763c56
Tyler Yu <tyleryy@uci.edu>
2024-04-20 15:14:16 -0700

    bg purple everywhere

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { twMerge } from "tailwind-merge";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="bg-purple-200">
+      <body className={twMerge(inter.className)}>{children}</body>
     </html>
   );
 }
