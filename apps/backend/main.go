88ecb5c733fb2581870714b813f5101ecd39d2bd
Katy Huang <katyhuang360166@gmail.com>
2024-04-19 23:04:21 -0700

    feat: set up concurrently with go backend

---
+++ apps/backend/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world!"))
+	})
+	http.ListenAndServe(":8080", nil)
+}
