d3a0ab3496609c253dfbc86be22ecdae60a0d6ba
Tyler Yu <tyleryy@uci.edu>
2024-04-20 15:21:46 -0700

    feat: waiting host page

---
+++ apps/web/app/game/waiting-host/page.tsx
@@ -0,0 +1,9 @@
+const WaitingRoomHostPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <span className="text-7xl">Waiting for Host...</span>
+    </div>
+  );
+};
+
+export default WaitingRoomHostPage;
---
+++ apps/web/app/game/waiting-player/page.tsx
@@ -0,0 +1,5 @@
+const WaitingRoomPlayerPage = () => {
+  return <></>;
+};
+
+export default WaitingRoomPlayerPage;
