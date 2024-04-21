b492781bbf7e7758282558dabff399bbc414cb21
Steven Zhou <stevenzhouni@gmail.com>
2024-04-20 17:15:04 -0700

    working on avatar selection

--- apps/web/app/selection/[roomCode]/page.tsx
+++ apps/web/app/selection/[roomCode]/page.tsx
@@ -1,9 +1,13 @@
 "use client";
+import Image from "next/image";
 
-const NameSelectionPage = ({params}) => {
+const NameSelectionPage = ({ params }) => {
   return (
     <main className="flex justify-center items-center bg-primary w-screen h-screen bg-purple-200">
-      <h1 className="text-7xl	font-bold">Pick Your Character! in room {params.roomCode}</h1>
+      <h1 className="text-7xl	font-bold">Pick Your Avatar!</h1>
+      <div>
+        
+      </div>
     </main>
   );
 };
