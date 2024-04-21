0cb50ed71a0bf92a59c65a5dce616b7b4a979449
Steven Zhou <stevenzhouni@gmail.com>
2024-04-20 18:50:26 -0700

    switched to framer

--- apps/web/app/loading.tsx
+++ apps/web/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center flex-col bg-primary w-screen h-screen bg-purple-200">
+    <div className="flex justify-center items-center flex-col bg-primary w-screen h-screen">
       <span className="loading loading-spinner loading-lg"></span>
     </div>
   );
--- apps/web/app/selection/[roomCode]/page.tsx
+++ apps/web/app/selection/[roomCode]/page.tsx
@@ -5,89 +5,39 @@ import DogStamp from "../../../assets/DogStamp.svg";
 import CatStamp from "../../../assets/CatStamp.svg";
 import PenguinStamp from "../../../assets/PenguinStamp.svg";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 interface PageProps {
   params: any;
 }
 
 const NameSelectionPage = ({ params }: PageProps) => {
-  const [isHoverChicken, setIsHoverChicken] = useState(false);
-  const [isHoverCat, setIsHoverCat] = useState(false);
-  const [isHoverDog, setIsHoverDog] = useState(false);
-  const [isHoverPenguin, setIsHoverPenguin] = useState(false);
-
   return (
-    <main className="flex justify-center items-center flex-col bg-primary w-screen h-screen bg-purple-200">
+    <main className="flex justify-center items-center flex-col w-screen h-screen">
       <h1 className="text-7xl	font-bold">Pick Your Avatar!</h1>
-      <div className="flex gap-8 pt-5">
-        <div
-          className="cursor-pointer"
-          onMouseOver={() => setIsHoverChicken(true)}
-          onMouseLeave={() => setIsHoverChicken(false)}
-        >
+      <div className="flex gap-8 pt-10 h-52">
+        <motion.div className="cursor-pointer" whileHover={{ scale: 1.2 }}>
           <Image
             src={ChickenStamp}
             alt="chicken-stamp"
-            width={isHoverChicken ? 200 : 125}
+            width={125}
             height={100}
-            style={
-              isHoverChicken
-                ? { filter: "drop-shadow(0px 15px 4px #ededed)" }
-                : undefined
-            }
           />
-        </div>
-        <div
-          className="cursor-pointer"
-          onMouseOver={() => setIsHoverCat(true)}
-          onMouseLeave={() => setIsHoverCat(false)}
-        >
-          <Image
-            src={CatStamp}
-            alt="cat-stamp"
-            width={isHoverCat ? 200 : 125}
-            height={100}
-            style={
-              isHoverCat
-                ? { filter: "drop-shadow(0px 15px 4px #ededed)" }
-                : undefined
-            }
-          />
-        </div>
-        <div
-          className="cursor-pointer"
-          onMouseOver={() => setIsHoverDog(true)}
-          onMouseLeave={() => setIsHoverDog(false)}
-        >
-          <Image
-            src={DogStamp}
-            alt="dog-stamp"
-            width={isHoverDog ? 200 : 125}
-            height={100}
-            style={
-              isHoverDog
-                ? { filter: "drop-shadow(0px 15px 4px #ededed)" }
-                : undefined
-            }
-          />
-        </div>
-        <div
-          className="cursor-pointer"
-          onMouseOver={() => setIsHoverPenguin(true)}
-          onMouseLeave={() => setIsHoverPenguin(false)}
-        >
+        </motion.div>
+        <motion.div className="cursor-pointer" whileHover={{ scale: 1.2 }}>
+          <Image src={DogStamp} alt="dog-stamp" width={125} height={100} />
+        </motion.div>
+        <motion.div className="cursor-pointer" whileHover={{ scale: 1.2 }}>
+          <Image src={CatStamp} alt="cat-stamp" width={125} height={100} />
+        </motion.div>
+        <motion.div className="cursor-pointer" whileHover={{ scale: 1.2 }}>
           <Image
             src={PenguinStamp}
             alt="penguin-stamp"
-            width={isHoverPenguin ? 200 : 125}
+            width={125}
             height={100}
-            style={
-              isHoverPenguin
-                ? { filter: "drop-shadow(0px 15px 4px #ededed)" }
-                : undefined
-            }
           />
-        </div>
+        </motion.div>
       </div>
     </main>
   );
