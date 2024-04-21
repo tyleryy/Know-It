497f3d329c40359a8aece4f2b67af5a4675bd991
Katy Huang <katyhuang360166@gmail.com>
2024-04-20 20:03:18 -0700

    feat: GenerateQuestionsFromImage helper function

--- apps/backend/services/questions/generate.go
+++ apps/backend/services/questions/generate.go
@@ -3,6 +3,9 @@ package questions
 import (
 	"backend/services"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/google/generative-ai-go/genai"
 )
@@ -40,5 +43,60 @@ func GenerateQuestionsFromText(count int, roomID string, resources string) *gena
 	return response
 }
 
-//TODO: GenerateQuestionsFromImage
-// func GenerateQuestionsFromImage(coun)
+// resource will be the path to the img directory (data/33/images)
+func GenerateQuestionsFromImage(count int, roomID string, resource string) *genai.GenerateContentResponse {
+	ctx, model := services.GetModel("gemini-pro-vision")
+	var images [][]byte
+
+	// walk through the img dir and load up the images
+	err := filepath.Walk(resource, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() && (filepath.Ext(path) == ".jpg" || filepath.Ext(path) == ".jpeg") {
+			data, err := os.ReadFile(path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			images = append(images, data)
+		}
+
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("Error reading directory: %s\n", err)
+	}
+
+	// construct prompt
+	var prompt []genai.Part
+
+	for _, imgData := range images {
+		prompt = append(prompt, genai.ImageData("jpeg", imgData))
+	}
+
+	text_prompt := fmt.Sprintf(`Generate a list of %d questions based on the images resources. Use the JSON format
+	given below.
+	{"id": generate a new uuid,
+	roomId:%s,
+	"prompt":"What does this course explore?",
+	"answer": "The course explores the significant philosophical implications of interdisciplinary approaches.",
+	"explanation":"The introduction states that the course 'explores...significant philosophical implications."
+	}
+	{"id": generate a new uuid,
+	roomId:%s,
+	"prompt":"What is the key benefit of an interdisciplinary approach?",
+	"answer": "An interdisciplinary approach combines philosophy and other disciplines, which impacts our understanding of ethics and morality.",
+	"explanation":"Key takeaway 1 states that 'Combines philosophy...impacting our understanding of ethics and morality."
+	}
+	`, count, roomID, roomID)
+
+	prompt = append(prompt, genai.Text(text_prompt))
+
+	response, err := model.GenerateContent(ctx, prompt...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return response
+
+}
