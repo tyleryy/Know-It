20b93daf2f78e71e8f44f36c987856e76b6e23b4
Steven Zhou <stevenzhouni@gmail.com>
2024-04-20 19:18:18 -0700

    added routing *waiting on room lobby to finish for dynamic routing

--- apps/web/app/selection/[roomCode]/page.tsx
+++ apps/web/app/selection/[roomCode]/page.tsx
@@ -6,19 +6,24 @@ import CatStamp from "../../../assets/CatStamp.svg";
 import PenguinStamp from "../../../assets/PenguinStamp.svg";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { navigateToRoomSetting } from "./actions";
 
 interface PageProps {
   params: any;
 }
 
 const NameSelectionPage = ({ params }: PageProps) => {
+  const [avatar, setAvatar] = useState("");
 
-  
   return (
     <main className="flex justify-center items-center flex-col w-screen h-screen">
       <h1 className="text-7xl	font-bold">Pick Your Avatar!</h1>
       <div className="flex gap-8 pt-10 h-52">
-        <motion.div className="cursor-pointer" whileHover={{ scale: 1.2 }}>
+        <motion.div
+          className="cursor-pointer"
+          whileHover={{ scale: 1.2 }}
+          onClick={() => setAvatar("chicken")}
+        >
           <Image
             src={ChickenStamp}
             alt="chicken-stamp"
@@ -26,13 +31,25 @@ const NameSelectionPage = ({ params }: PageProps) => {
             height={100}
           />
         </motion.div>
-        <motion.div className="cursor-pointer" whileHover={{ scale: 1.2 }}>
+        <motion.div
+          className="cursor-pointer"
+          whileHover={{ scale: 1.2 }}
+          onClick={() => setAvatar("dog")}
+        >
           <Image src={DogStamp} alt="dog-stamp" width={125} height={100} />
         </motion.div>
-        <motion.div className="cursor-pointer" whileHover={{ scale: 1.2 }}>
+        <motion.div
+          className="cursor-pointer"
+          whileHover={{ scale: 1.2 }}
+          onClick={() => setAvatar("cat")}
+        >
           <Image src={CatStamp} alt="cat-stamp" width={125} height={100} />
         </motion.div>
-        <motion.div className="cursor-pointer" whileHover={{ scale: 1.2 }}>
+        <motion.div
+          className="cursor-pointer"
+          whileHover={{ scale: 1.2 }}
+          onClick={() => setAvatar("penguin")}
+        >
           <Image
             src={PenguinStamp}
             alt="penguin-stamp"
@@ -41,6 +58,13 @@ const NameSelectionPage = ({ params }: PageProps) => {
           />
         </motion.div>
       </div>
+      <button
+        className="btn btn-success mt-10"
+        onClick={() => navigateToRoomSetting(params.roomCode)}
+        disabled={!avatar}
+      >
+        Continue{" "}
+      </button>
     </main>
   );
 };
