b25d296148d95585381c96e3e6c804097414e6be
Tyler Yu <tyleryy@uci.edu>
2024-04-20 19:15:33 -0700

    feat: file upload bugs fixed

--- apps/web/app/game/lobby/page.tsx
+++ apps/web/app/game/lobby/page.tsx
@@ -4,13 +4,29 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import file_img from "./file-1453.svg";
 
+import dog from "../../../assets/avatars/dog.svg";
+import frog from "../../../assets/avatars/frog.svg";
+import penguin from "../../../assets/avatars/penguin.svg";
+import chicken from "../../../assets/avatars/chicken.svg";
+
+const userConfig = [
+  { name: "tyler" },
+  { name: "nwin" },
+  { name: "stevem" },
+  { name: "katy" },
+];
+
+const avatarMap: any = [dog, frog, penguin, chicken];
+
 const LobbyPage = () => {
   return (
     <>
       <div className="flex justify-center items-center h-screen flex-col">
         <div className="flex flex-row justify-center">
           <div className="flex flex-row gap-10">
-            <div>hi</div>
+            <div>
+              <AvatarStack users={userConfig} />
+            </div>
             <div>
               <FileUpload />
             </div>
@@ -24,6 +40,26 @@ const LobbyPage = () => {
 
 export default LobbyPage;
 
+const AvatarStack = ({ users }: any) => {
+  return (
+    <>
+      {users.map((user: any) => {
+        return (
+          <div key={user.name} className="relative -left-10">
+            <Image
+              src={avatarMap[Math.floor(Math.random() * avatarMap.length)]}
+              alt="player avatar"
+              height={100}
+              width={100}
+            />
+            {user.name}
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
 const FileUpload = () => {
   const [files, setFiles] = useState<any>([]);
 
@@ -32,7 +68,12 @@ const FileUpload = () => {
   // });
 
   const handleUpload = async () => {
+    if (!uploadFile) {
+      console.log("no file to upload");
+      return;
+    }
     setFiles([...files, uploadFile]);
+    setUploadFile(null);
   };
   const [uploadFile, setUploadFile] = useState<any>(null);
   return (
@@ -48,8 +89,8 @@ const FileUpload = () => {
         })}
       </div>
       <div className="card-body items-center text-center">
-        <h2 className="card-title">Files!</h2>
-        <p>Upload Documentation</p>
+        <h2 className="card-title">Upload Documentation</h2>
+        <p>Study Guides, Notes, and more!</p>
         <div className="card-actions">
           <input
             type="file"
@@ -58,7 +99,12 @@ const FileUpload = () => {
             }}
             className="file-input file-input-bordered file-input-info w-full max-w-xs bg-white"
           />
-          <button className="btn btn-primary" onClick={handleUpload}>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              handleUpload();
+            }}
+          >
             Upload File
           </button>
         </div>
