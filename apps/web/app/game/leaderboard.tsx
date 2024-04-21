08bba5d9dab9dba38fc04dd1ae2c2bcdb30f9d20
Dang Nguyen Nguyen <93059380+NwinNwin@users.noreply.github.com>
2024-04-20 17:21:59 -0700

    feat: implemented leaderboard, chatbox with fake data

---
+++ apps/web/app/game/leaderboard.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import dog from "../../assets/avatars/dog.svg";
+import frog from "../../assets/avatars/frog.svg";
+import penguin from "../../assets/avatars/penguin.svg";
+import chicken from "../../assets/avatars/chicken.svg";
+import Image from "next/image";
+
+const FAKEUSERS = [
+  {
+    username: "GreenHopper",
+    avatar: "frog",
+    points: 12,
+  },
+  {
+    username: "BarkRanger",
+    avatar: "dog",
+    points: 13,
+  },
+  {
+    username: "IceWaddler",
+    avatar: "penguin",
+    points: 15,
+  },
+  {
+    username: "FeatherFan",
+    avatar: "chicken",
+    points: 11,
+  },
+];
+
+const avatarMap = {
+  dog: dog,
+  frog: frog,
+  penguin: penguin,
+  chicken: chicken,
+};
+
+const sortedUsers = FAKEUSERS.sort((a, b) => b.points - a.points);
+
+export default async function Leaderboard() {
+  const leaderboardResult = sortedUsers.map((user, index) => (
+    <div
+      key={index}
+      className="flex items-center justify-between px-4 py-2 rounded-lg bg-[linear-gradient(145deg,_#202c3f,_#1b2535)]"
+    >
+      <div className="flex items-center space-x-2">
+        <Image
+          className="w-9 h-9 rounded-full"
+          src={avatarMap[user.avatar]}
+          alt={user.avatar}
+        />
+        <span className="font-medium">{user.username}</span>
+      </div>
+      <span className="text-lg font-semibold">{user.points}</span>
+    </div>
+  ));
+
+  return (
+    <div className="flex flex-col w-2/12 h-full bg-[#141C2F] rounded-lg text-white">
+      <h1 className="text-center text-xl font-bold py-4">Leaderboard</h1>
+      <div className="flex flex-col gap-5 px-2">{leaderboardResult}</div>
+    </div>
+  );
+}
--- apps/web/app/game/page.tsx
+++ apps/web/app/game/page.tsx
@@ -1,5 +1,31 @@
-const GamePage = () => {
-    return <></>
-}
+import { redirect } from "next/navigation";
+import { createClient } from "../../utils/supabase/server";
+import Leaderboard from "./leaderboard";
+import AiGame from "./aiGame";
+import Chat from "./chat";
 
-export default GamePage;
+export default async function Page() {
+  //   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  //   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  //   const supabase = createClient(supabaseUrl, supabaseKey);
+
+  //   const { data, error } = await supabase.auth.getUser();
+  //   const { user } = data;
+
+  //   if (error) {
+  //     console.log(error);
+  //     redirect("/login");
+  //   }
+
+  //   const { data: users } = await supabase.from("Users").select("");
+
+  return (
+    <main className="flex bg-[#19263F] justify-center items-center bg-primary w-screen h-screen ">
+      <div className="flex w-full h-[70vh] justify-center items-center gap-7">
+        <Leaderboard />
+        <AiGame />
+        <Chat />
+      </div>
+    </main>
+  );
+}
