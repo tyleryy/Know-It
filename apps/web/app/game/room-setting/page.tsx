38f6e9ded1bccdc727d6d132b8c129aa725dac11
Tyler Yu <tyleryy@uci.edu>
2024-04-20 17:59:53 -0700

    feat: ready button

--- apps/web/app/game/room-setting/page.tsx
+++ apps/web/app/game/room-setting/page.tsx
@@ -1,38 +1,7 @@
 "use client";
 
-import { use, useEffect, useState } from "react";
-import { twMerge } from "tailwind-merge";
-
-// const QuestionRange = ({ value, setValue, min, max, className }) => {
-//   useEffect(() => {
-//     console.log(value);
-//   });
-//   return (
-//     <>
-//       <span className="text-5xl text-black underline ">
-//         Number of Questions
-//       </span>
-//       <div>
-//         <input
-//           type="range"
-//           min={min}
-//           max={max}
-//           value={value}
-//           onChange={(event) => setValue(event.target.value)}
-//           className={twMerge("range", className)}
-//           step={max / 4}
-//         />
-//         <div className="w-full flex justify-between text-xs px-2 text-black">
-//           <span>| 5</span>
-//           <span>| 10</span>
-//           <span>| 15</span>
-//           <span>| 20</span>
-//           {/* <span>|</span> */}
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
+import { useState } from "react";
+import ReadyButton from "./ReadyButton";
 
 const RoomSetting = () => {
   // Initialize state with the default value 25
@@ -88,23 +57,7 @@ const RoomSetting = () => {
           </div>
         </>
       </div>
-
-      <button className="btn btn-default text-black bg-white m-10 w-48 h-16 border-none hover:text-white hover:bg-purple-800 rounded-3xl">
-        Ready
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="stroke-current shrink-0 h-6 w-6 text-green-500"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      </button>
+      <ReadyButton label="Ready" onClick={() => {}} />
     </div>
   );
 };
