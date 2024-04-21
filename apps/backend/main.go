e165259cb5d9e77b6393e5e3335a27f4bb960a21
Katy Huang <katyhuang360166@gmail.com>
2024-04-21 05:31:18 -0700

    feat: allow origins

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"backend/api/upload"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		
+		AllowAllOrigins: true,
+	}))
+
 	router.POST("/api/upload/text/:roomId/:count", upload.HandleTextUpload)
 
 	router.POST("/api/upload/file/:roomId/:count", upload.HandleFileUpload)
